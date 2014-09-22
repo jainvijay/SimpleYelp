8b54615684ffcc6ae143f697a3c2d05cdf234c07
Vijayjain <jvijay@yahoo-inc.com>
2014-09-21 19:20:43 -0700

    Initial Commit

---
+++ SimpleYelp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleYelp
+//
+//  Created by Vijay Jain on 9/21/14.
+//  Copyright (c) 2014 Vijay Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleYelpTests/SimpleYelpTests.m
@@ -0,0 +1,34 @@
+//
+//  SimpleYelpTests.m
+//  SimpleYelpTests
+//
+//  Created by Vijay Jain on 9/21/14.
+//  Copyright (c) 2014 Vijay Jain. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SimpleYelpTests : XCTestCase
+
+@end
+
+@implementation SimpleYelpTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
